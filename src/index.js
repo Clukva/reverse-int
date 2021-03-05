efdc0ee8575a01679d1dba85bcb0514874451ea1
Clukva <yura4-1-1@mail.ru>
2021-03-06 00:00:18 +0300

    feat: add solution Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    if(n > 0){
+    let rev = +n.toString().split("").reverse().join("") 
+    
+  return rev
+}else if ( n < 0){
+    let rev2 = Math.abs(n).toString().split("").reverse().join("")
+     
+    return rev2
+}
+    
+
 }
